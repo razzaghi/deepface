16ea10d3926e8988259a635a0c85be45180d1036
mrrazzaghi <razzaghi229@gmail.com>
2022-01-15 18:46:34 +0330

    bugfix

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,13 +1,4 @@
-import argparse
-
 from api import app
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-p', '--port',
-        type=int,
-        default=5000,
-        help='Port of serving api')
-    args = parser.parse_args()
-    app.run(host='0.0.0.0', port=args.port)
+    app.run()
