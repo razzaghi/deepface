34ea9bb3fbf9439fe1274e6ad85603d0b9ed0ff7
mrrazzaghi <razzaghi229@gmail.com>
2022-01-15 18:38:49 +0330

    fix file structure

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,4 +1,13 @@
+import argparse
+
 from api import app
 
 if __name__ == "__main__":
-    app.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p', '--port',
+        type=int,
+        default=5000,
+        help='Port of serving api')
+    args = parser.parse_args()
+    app.run(host='0.0.0.0', port=args.port)
